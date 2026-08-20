5566de4a47b54239716917a2c5969844931ff942
luiss-tech <luisss31.2006@gmail.com>
2026-08-20 12:23:33 -0500

    Remove static GitHub Pages output

--- docs/assets/js/layout.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const mobileToggleItems = document.querySelectorAll('.sidebar-link');
-  mobileToggleItems.forEach((item) => item.addEventListener('click', (event) => event.preventDefault()));
-});
